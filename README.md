75a6633d5cb768dc0c213617ea4032ac1de7e622
Tomasz Seweryn <tomasz.seweryn@crealogix.com>
2017-11-18 16:19:14 +0100

    Preparation

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# workshop_L1
+Budget Tool with React Bootstrap
+
+Prerequisites
+1. Install nodejs : https://nodejs.org/en/download/ . Nodejs is a server for our case study.
+2. (Recommended) Install Visual Studio Code (VSC): https://code.visualstudio.com/download.
+You can develop in any favourite editor or IDE, but also learn about VSC, which has some fancy features
+making development in Javascript attractive.
+3. (Recommended) Download Chrome web browser. Enable debugging in VSC with the support of
+Chrome web browser : https://code.visualstudio.com/docs/editor/debugging
+4. Run in the root folder of the project : npm install (In VSC you can do it from an integrated terminal : View->Integrated Terminal).
+This will download all needed dependencies for the project.
+5. Run in the root folder of the project : npm start
+This will start the application and open it in your default web browser
+
+Workshop tasks
+1. Validation is not perfect in the app. Improve it. When introducing new validations, just save the source code and see how nodejs
+refreshes the application status in the browser automatically. Try adding new participants when working on validations. Testing this
+will be helpful for solving problem number 2.
+	a) Currently, a phone number can be any text. By using a proper function from 'libphonenumber-js' library add validation
+	inside "handleAddParticipant" function of CreateSplitView.js. Take care of validation message everywhere in the code where necessary.
+	See examples of existing validations in CreateSplitView.js.
+	hints : 
+		to add a dependency to the project -> npm install <library_name> --save
+		to import a library -> see existing imports in CreateSplitView.js
+		libphonenumber-js code snippets : 
+			https://www.npmjs.com/package/libphonenumber-js#usage
+			https://www.npmjs.com/package/libphonenumber-js#isvalidnumberparsed_number
+	b) Participant contribution can be any text. Add validation, so that only positive number or 0 will be allowed 
+	hints : 
+		look for javascript built-in functions helping with checking if something is a number.
+	c) "this.state.amount" and "this.state.myContribution" also need validation improvements. They should be only of positive value.
+	"this.state.amount" can't be 0, while "this.state.myContribution" can.
+	hints : 
+		The logic responsible for validations of these two fields should be inside "handleCalculate" function in CreateSplitView.js
+
+2.	Probably you noticed (if not, this is ok too) when testing addition of participants and changing validations, that when the source code is saved and the app
+refreshed in the browser, the previously added participants disappear from the list at the bottom of the page. This happens, because
+during mounting the CreateSplitView component, that extends Component object from React library, does not load the saved data. This is
+a bug! Fix it, so that this data will be loaded before the function "render" is called in the lifecycle of the component.
+How to reproduce the bug : 
+	1. Add new participants
+	2. When adding second participant, provide wrong value in validated fields
+	3. Try to add this second participant and tigger a validation message
+	4. Add anywhere in the source code of CreateSplitView.js a comment : /* some comment */
+	5. Save CreateSplitView.js and see how the page after refresh looses participants from the list
+	6. Adding new participant without validation errors shows all participants again
+	hints : 
+		to find a proper method to place the fix in -> https://reactjs.org/docs/react-component.html
+		"this.state.allParticipants" needs to be set after loading the data from "localStorage". Look for the way data is retrieved
+		from localStorage and allParticipants are set. Examples already exist in CreateSplitView.js. To set a value in state object
+		you can use this.setState({ <key> : <object> }); , where values in "<...>" brackets should be replaced by you properly (together
+		with these brackets).
+				
+3.	After successful providing of the contribution data of all participants we press "Calculate" button and get to the page with the
+summary of costs and information on who owes whom the money. The way this data presentation is programmed (strings concatenation)
+is not flexible and to make development easier and friendly to other devs who might work with this code in future you should change
+it to a tabelaric way of presentation. Change the contribution data and costs data to be displayed as tables.
+	hints :
+		https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+		Add a new function to ExpensesView.js, which will accept four parameters in input : 
+			sourceUser (user who either owes someone money or gets money from the pool),
+			action (text describing the activity happening. Either owning or getting money),
+			destinationUser (either user who receives money from other user or hardcoded value "the pool"),
+			amount (the amount necessary to be transfered).
+		This function should construct an object with four properties storing parameter values from the input and return this object.
+		Modify "result.push(...)" calls to use your newly added function instead of a concatenation of strings.
+		See "Participants overview" table source in CreateSplitView.js for an example on how to construct the data for the table body.
+	
+	
---
+++ src/ExpensesView.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Jumbotron, Grid, Row, Col} from 'react-bootstrap';
+
+import Header from './Header';
+import './App.css';
+
+class ExpensesView extends Component {
+  static contextTypes = {
+    router: PropTypes.object,
+  };
+  state = {
+    totalAmount: '',
+    currency: '',
+    mycontribution: '',
+    allParticipants: '',
+  }
+
+  handleStart = () => {
+    //localStorage.clear();
+    this.context.router.history.push('/createsplit');
+  }
+  componentWillMount() {
+    const creator = JSON.parse(localStorage.getItem('creator'));
+    const participants = JSON.parse(localStorage.getItem('participants'));
+    this.setState({ allParticipants: participants });
+    console.log('data',localStorage);
+    this.setState({totalAmount: creator.totalAmount, currency: creator.currency, mycontribution:creator.creatorContribution});
+  }
+
+  calcuateShare () {
+    let result = [];
+    const numberOfParticipants = this.state.allParticipants.length + 1;
+    const participants = this.state.allParticipants;
+    let totalCost = this.state.totalAmount;
+    
+    // Add the user of the app to the list of participants
+    const participant = {};
+    participant.name = "User";
+    participant.email = "";
+    participant.phoneNumber = "";
+    participant.contribution = this.state.mycontribution;
+    participants.push(participant);
+
+    // Check if participants collected more than needed amount of money
+    let aboveTheCost = participants.map(item => Number(item.contribution)).reduce((prev, next) => prev + next) - totalCost;
+
+    // 1. Calculate fair cost per person
+    let share = totalCost / numberOfParticipants;
+
+    // 2. Calculate actual cost per person and save as map with persons names as keys
+    let mapOfCosts = new Map(participants.map((item) => [item.name, item.contribution-share]));    
+
+    // 3. Check if there was too much money collected and how much should be given to each contributor from the pool
+    participants.forEach(participant => {
+      if (mapOfCosts.get(participant.name) > 0 && aboveTheCost > 0) {
+        if (aboveTheCost > mapOfCosts.get(participant.name)) {
+          result.push(participant.name + " gets from the pool " + mapOfCosts.get(participant.name));
+          aboveTheCost -= mapOfCosts.get(participant.name);
+          mapOfCosts.set(participant.name, 0.0);
+        } else if (aboveTheCost > 0) {
+          result.push(participant.name + " gets from the pool " + aboveTheCost);
+          mapOfCosts.set(participant.name, mapOfCosts.get(participant.name) - aboveTheCost);
+          aboveTheCost = 0.0;
+        }
+      }
+    });
+
+    // 4. Calculate who owes whom and how much
+    participants.forEach(participantOuter => {
+      if (mapOfCosts.get(participantOuter.name) > 0) {
+        participants.forEach(participantInner => {
+          if (!(JSON.stringify(participantOuter) === JSON.stringify(participantInner)) && mapOfCosts.get(participantInner.name) < 0) {
+            if (mapOfCosts.get(participantOuter.name) > Math.abs(mapOfCosts.get(participantInner.name))) {
+              result.push(participantInner.name + " owes " + participantOuter.name + " " + Math.abs(mapOfCosts.get(participantInner.name)));
+              mapOfCosts.set(participantOuter.name, mapOfCosts.get(participantOuter.name) + mapOfCosts.get(participantInner.name));
+              mapOfCosts.set(participantInner.name, 0.0);
+            } else {
+              result.push(participantInner.name + " owes " + participantOuter.name + " " + Math.abs(mapOfCosts.get(participantOuter.name)));
+              mapOfCosts.set(participantOuter.name, 0.0);
+              mapOfCosts.set(participantInner.name, mapOfCosts.get(participantInner.name) + mapOfCosts.get(participantOuter.name));
+            }
+          }
+        })
+      }
+    });
+
+    return result;
+  }
+  render() {
+    const share = this.calcuateShare().map((item, index) =>(
+      <h3 key={index}>{item}</h3>
+    ));
+    return (
+      <div className="App">
+        <Header />
+        <Grid>
+          <Row>
+            <Col xs={12} md={12}>
+              <Jumbotron>
+                <h1>Overview of expenses</h1>
+              </Jumbotron>
+            </Col>
+          </Row>
+          <Row className="text-left">
+            <Col xs={12} md={12}>
+              <Jumbotron>
+                <h2>
+                The group spent a total amount of &nbsp;
+                <strong>{this.state.currency}&nbsp;{this.state.allParticipants.map(item => Number(item.contribution)).reduce((prev, next) => prev + next)}</strong>
+                .</h2>
+                <h2>
+                The cost of shopping was &nbsp;
+                <strong>&nbsp;{this.state.totalAmount}</strong>
+                .</h2>
+                <h2>
+                  The following people paid : &nbsp;
+                </h2>
+                {this.state.allParticipants.map(item =>(
+                  <h3 key={item.name}>
+                    {item.name} contributed {item.contribution}
+                  </h3>
+                ))}                 
+                <h2>
+                  Costs should be split the following way
+                </h2>
+                {share}
+              </Jumbotron>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default ExpensesView;
